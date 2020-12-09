c07715662b13c38cfc0e72b8945ac585e4daebe0
DevOsamaIslam <osamasamarrai@gmail.com>
2020-12-09 19:35:18 +0800

    add language support

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 import express from 'express'
 const router = express.Router()
+import Language from '../model/Language.js'
+
+let defaultLang = {
+	lang: 'en',
+	language: 'English',
+	script: 'English',
+	contents: {
+		tabs: {
+			url: {
+				tabTitle: 'URL',
+				placeholder: 'Type the URL'
+			},
+			text: {
+				tabTitle: 'Text',
+				placeholder: 'Paste your text here'
+			},
+			file: {
+				tabTitle: 'File',
+				placeholder: 'File size must be below 30 MB',
+				addButton : 'File',
+				progressText : 'done..'
+			}
+		},
+		submitButton: 'Drop'
+	}
+}
 
 /* GET home page. */
 router.get('/', (req, res) => {
-	res.render('index')
+	let accepted = req.locale.language
+	Language.findOne({ lang: accepted }, (err, data) => {
+		res.render('index', {language: data || defaultLang})
+	})
 })
 
 export default router
